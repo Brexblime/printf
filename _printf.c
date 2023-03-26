a2d06222d7ea0264fa448c39abc029d87f380c2a
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-26 22:38:33 +0000

    Fixed: files conflicted and removed the struct match form main.h

--- _printf.c
+++ _printf.c
@@ -34,8 +34,8 @@ Beginning:
 			j = 2;
 			while (j >= 0)
 			{
-				if (magic[j].formater[0] == format[i] && 
-						magic[j].formater[1] == format[i + 1]
+				if (magic[j].formater[0] == format[i] 
+				&& magic[j].formater[1] == format[i + 1])
 				{
 					len += magic[j].func(args);
 					i = i + 2;
