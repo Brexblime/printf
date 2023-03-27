2f343e9edabdbc2707ff35b84dc0d7fcd7fd1c4a
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-27 01:00:14 +0000

    Add: print_precent file

---
+++ print_percent.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_percent - print %
+ * @arg: list of arguments
+ * Return: 1
+*/
+int print_percent(va_list arg)
+{
+	(void) arg; /* unused */
+	_putchar('%');
+	return (1);
+}
