93a5200885f611cddb0130fc4bd6bf64044d1e6e
Brexblime <jaouhari.bahaadine@gmail.com>
2023-03-28 02:18:03 +0000

    adding the flag 0(ZERO)

--- print_number.c
+++ print_number.c
@@ -42,22 +42,23 @@ int print_number(va_list arg)
 
 	return (i);
 }
-
 /**
  * print_decimal - prints decimal
  * @arg: argument to print
+ * @flags: struct containing formatting flags
  * Return: number of characters printed
  */
 
-int print_decimal(va_list arg)
+int print_decimal(va_list arg, flags_t *flags)
 {
 	int n = va_arg(arg, int);
 	int num, last = n % 10, digit;
-	int  i = 1;
-	int exp = 1;
+	int i = 1, exp = 1;
 
 	n = n / 10;
 	num = n;
+	if (flags->zero_padding && !flags->minus_flag)
+		i += print_padding('0', flags->width - 1);
 
 	if (last < 0)
 	{
@@ -74,17 +75,18 @@ int print_decimal(va_list arg)
 			exp = exp * 10;
 			num = num / 10;
 		}
-		num = n;
-		while (exp > 0)
-		{
-			digit = num / exp;
-			_putchar(digit + '0');
-			num = num - (digit * exp);
-			exp = exp / 10;
-			i++;
-		}
+	num = n;
+	while (exp > 0)
+	{
+		digit = num / exp;
+		_putchar(digit + '0');
+		num = num - (digit * exp);
+		exp = exp / 10;
+		i++;
+	}
 	}
 	_putchar(last + '0');
-
+	if (flags->minus_flag)
+		i += print_padding(' ', flags->width - 1);
 	return (i);
 }
